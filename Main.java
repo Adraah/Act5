1aea837fbd59653d1bf77abffd76245bb15e0044
Adraah <80177793+Adraah@users.noreply.github.com>
2022-09-06 19:41:31 -0500

    Commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int valor1 = 6;
+        int valor2 = 2;
+
+        System.out.println("X: "+ valor1);
+        System.out.println("Y: "+ valor2);
+        System.out.println("Suma: " + (valor1 + valor2));
+        System.out.println("Resta: " + (valor1 - valor2));
+        System.out.println("Multiplicacion: " + (valor1 * valor2));
+        System.out.println("Division: " + (valor1 / valor2));
+        System.out.println("Modulo: " + (valor1 % valor2));
+    }
+}
